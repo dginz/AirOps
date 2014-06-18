b77715e5c1e680a0dcb0563e931c11b559da9cf0
issimo <seano@intellissimo.com>
2014-06-18 16:29:56 +0100

    Wednesday End

--- js/custom.js
+++ js/custom.js
@@ -11,32 +11,55 @@ $(document).ready(function(){
 		document.getElementById("utcTime").innerHTML=d.getUTCHours()+':'+ utcM;
 		document.getElementById("sumDateN").innerHTML=d.getDate()+' '+ months[xMonth] + ' ' + d.getFullYear();
 
-	$("#aDfct>button").on("click", function(){$("#nDfct").show("fast")});
+	$("#aDfct>button").on("click", function(){$("#nDfct").show("fast");return false;});
 	$("#stButt").on("click",function(){
 		$("#vOper,#vMaint,#vSumm").hide("fast");
 		$("#vStart").show("slow");
 		$("ol.breadcrumb span").attr("class","glyphicons glyphicons-nameplate")
 		$("ol.breadcrumb li:nth-child(2)").html("Startup");
+		return false;
 		});
 	$("#mButt").on("click",function(){
 		$("#vOper,#vStart,#vSumm").hide("fast");
 		$("#vMaint").show("slow");
 		$("ol.breadcrumb span").attr("class","glyphicons glyphicons-settings")
 		$("ol.breadcrumb li:nth-child(2)").html("Maintenance");
+		return false;
 		});
 	$("#oButt").on("click",function(){
 		$("#vStart,#vMaint,#vSumm").hide("fast");
 		$("#vOper").show("slow");
 		$("ol.breadcrumb span").attr("class","glyphicons glyphicons-airplane")
 		$("ol.breadcrumb li:nth-child(2)").html("Operational");
+		return false;
 		});
 	$("#suButt").on("click",function(){
 		$("#vOper,#vMaint,#vStart").hide("fast");
 		$("#vSumm").show("slow");
 		$("ol.breadcrumb span").attr("class","glyphicons glyphicons-notes");
 		$("ol.breadcrumb li:nth-child(2)").html("Summary");
+		return false;
 		});
-		$(".cfm").on("click",function(){
-			$(this).attr("class","cfmD").html("CONFIRMED");
-			});
+	
+/*StartupScript*/
+	$("#fConf .cfm").on("click",function(){$(this).attr("class","cfmD").html("CONFIRMED");$("#fConf .cirStatW").attr("class","cirStat").html("OK");$("#fConf input, #fConf select").attr("disabled","disabled");return false;});
+	
+	$("#fConf .edBtn").on("click",function(){$("#fConf .cfmD").attr("class","cfm").html("CONFIRM");$("#fConf .cirStat").attr("class","cirStatW");$("#fConf input, #fConf select").removeAttr("disabled");return false});
+	
+	$("#fCrew .cfm").on("click",function(){$(this).attr("class","cfmD").html("CONFIRMED");$("#fCrew .cirStatW").attr("class","cirStat").html("OK");$("#fCrew input, #fCrew select").attr("disabled","disabled");return false;});
+	
+	$("#fCrew .edBtn").on("click",function(){$("#fCrew .cfmD").attr("class","cfm").html("CONFIRM");$("#fCrew .cirStat").attr("class","cirStatW");$("#fCrew input, #fCrew select").removeAttr("disabled");return false;});		
+			
+	$("#fAirStat .cfm").on("click",function(){$(this).attr("class","cfmD").html("CONFIRMED");$("#fAirStat .cirStatW").attr("class","cirStat").html("OK");$("#fAirStat input, #fAirStat select").attr("disabled","disabled");return false;});
+	
+	$("#fAirStat .edBtn").on("click",function(){$("#fAirStat .cfmD").attr("class","cfm").html("CONFIRM");$("#fAirStat .cirStat").attr("class","cirStatW");$("#fAirStat input, #fAirStat select").removeAttr("disabled");return false;});		
+			
+	$("#fMulti .cfm").on("click",function(){$(this).attr("class","cfmD").html("CONFIRMED");$("#fMulti .cirStatW").attr("class","cirStat").html("OK");$("#fMulti input, #fMulti select").attr("disabled","disabled");return false;});
+	
+	$("#fMulti .edBtn").on("click",function(){$("#fMulti .cfmD").attr("class","cfm").html("CONFIRM");$("#fMulti .cirStat").attr("class","cirStatW");$("#fMulti input, #fMulti select").removeAttr("disabled");return false;});
 	});
+
+
+
+
+/*OperationalScript*/
